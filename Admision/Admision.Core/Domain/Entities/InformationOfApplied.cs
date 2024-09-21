ffbe371562c13f4ebb763dd882c34cad4f4e3051
VuVanThanh <vut4262@gmail.com>
2024-09-21 14:00:08 +0700

    update InformationOfApplied Model

---
+++ Admision/Admision.Core/Domain/Entities/InformationOfApplied.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admission.Core.Domain.Entities
+{
+	public class InformationOfApplied
+	{
+		public Guid Id { get; set; }
+	}
+}
