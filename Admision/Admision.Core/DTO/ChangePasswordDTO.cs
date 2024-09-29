4dadebf063a8789581032335d71fae9b9ebaf5a5
sacsigiau <sacsigiau@gmail.com>
2024-09-29 21:14:37 +0700

    updateviewchangepass

---
+++ Admision/Admision.Core/DTO/ChangePasswordDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Admission.Core.DTO
+{
+	public class ChangePasswordDTO
+	{
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu hiện tại.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mật khẩu hiện tại")]
+        public string? CurrentPassword { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu mới.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mật khẩu mới")]
+        [StringLength(100, ErrorMessage = "Mật khẩu phải có ít nhất {2} ký tự.", MinimumLength = 6)]
+        public string? NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng xác nhận mật khẩu mới.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Xác nhận mật khẩu mới")]
+        [Compare("NewPassword", ErrorMessage = "Mật khẩu mới và xác nhận mật khẩu không khớp.")]
+        public string? ConfirmNewPassword { get; set; }
+    }
+}
--- Admision/Admision.Core/DTO/LoginDTO.cs
+++ Admision/Admision.Core/DTO/LoginDTO.cs
@@ -4,16 +4,16 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Admission.Core.DTO
 {
- public class LoginDTO
- {
-  [Required(ErrorMessage = "Email không thể để trống!")]
-  [EmailAddress(ErrorMessage = "Email không đúng định dạng!")]
-  [DataType(DataType.EmailAddress)]
-  public string? Email { get; set; }
+	public class LoginDTO
+	{
+		[Required(ErrorMessage = "Email không thể để trống!")]
+		[EmailAddress(ErrorMessage = "Email không đúng định dạng!")]
+		[DataType(DataType.EmailAddress)]
+		public string? Email { get; set; }
 
 
-  [Required(ErrorMessage = "Password không thể để trống!")]
-  [DataType(DataType.Password)]
-  public string? Password { get; set; }
- }
+		[Required(ErrorMessage = "Password không thể để trống!")]
+		[DataType(DataType.Password)]
+		public string? Password { get; set; }
+	}
 }
