19d612aafdc83bb9fbd1d7412f09b05a1e41a617
buigiau <sacsigiau@gmail.com>
2024-10-06 22:21:40 +0700

    addviewcomponent

---
+++ Admision/Admision.UI/ViewModels/ArticleVM.cs
@@ -0,0 +1,11 @@
+﻿namespace Admission.UI.ViewModels
+{
+    public class ArticleVM
+    {
+        public Guid ArticleID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime DateCreate { get; set; }
+
+    }
+}
