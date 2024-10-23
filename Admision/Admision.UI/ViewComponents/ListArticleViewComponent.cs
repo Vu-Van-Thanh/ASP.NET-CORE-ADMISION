c92a95de85462294076d0d47113b9d85f4db960f
buigiau <sacsigiau@gmail.com>
2024-10-23 22:10:10 +0700

    updatehomeUI

---
+++ Admision/Admision.UI/ViewComponents/ListArticleViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Admission.UI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Admission.Core.DTO;
+using Admission.Core.Domain.Entities; 
+using System.Collections.Generic;
+using Admission.Core.ServiceContracts;
+
+namespace Admission.UI.ViewComponents
+{
+	public class ListArticleViewComponent : ViewComponent
+	{
+		private readonly IArticlesService _articleService; 
+		public ListArticleViewComponent(IArticlesService articleService)
+		{
+			_articleService = articleService;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+
+			var articles = await _articleService.GetAllArticle();
+
+			var articleDTOs = articles.Select(article => article.ToArticleDTO()).ToList();
+
+			return View(articleDTOs);
+		}
+	}
+}
