e65cb703119cdb48dff53f46e022a5e3627712bd
buigiau <sacsigiau@gmail.com>
2024-12-09 17:12:54 +0700

    VNPay

---
+++ Admision/Admision.UI/Controllers/RegisterExController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Admission.Core.DTO;
+
+namespace Admission.UI.Controllers
+{
+    public class RegisterExController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RegisterEx(RegisterExDTO registerExDTO)
+        {
+            return View();
+		}
+    }
+}
